a7a6bf32990f697f827b70b4c18e8a6bc09a3895
Arthur0292 <arthurvitordasilva01@gmail.com>
2025-11-07 20:53:06 -0300

    Initialize particles.js with JSON configuration

---
+++ app.js
@@ -0,0 +1,4 @@
+/* Inicializa o particles.js carregando a configuração JSON no elemento com ID 'particles-js' */
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('callback - particles.js config carregada');
+});
